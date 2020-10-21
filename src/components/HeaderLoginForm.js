b3150aa3203126c550b0421c03f5c7c80620ecba
Dewa <adewalade@gmail.com>
2020-10-21 19:23:10 +0100

    [feat] connect Header login form to backend
    Connected the header login form to the backend server for
    user authorization.

--- src/components/HeaderLoginForm.js
+++ src/components/HeaderLoginForm.js
@@ -1,14 +1,34 @@
-import React from "react";
+import Axios from "axios";
+import React, { useState } from "react";
 
 function HeaderLoginForm() {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await Axios.post("http://localhost:8080/login", { username, password });
+
+      if (response.data) {
+        console.log(response.data);
+      } else {
+        console.log("[Error] incorrect username or password.");
+      }
+    } catch (e) {
+      console.log("[Error] Couldn't Log user in");
+    }
+  }
+
   return (
-    <form className="mb-0 pt-2 pt-md-0">
+    <form onSubmit={handleLogin} className="mb-0 pt-2 pt-md-0">
       <div className="row align-items-center">
         <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-          <input name="username" className="form-control form-control-sm input-dark" type="text" placeholder="Username" autoComplete="off" />
+          <input onChange={(e) => setUsername(e.target.value)} name="username" className="form-control form-control-sm input-dark" type="text" placeholder="Username" autoComplete="off" />
         </div>
         <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-          <input name="password" className="form-control form-control-sm input-dark" type="password" placeholder="Password" autoComplete="off" />
+          <input onChange={(e) => setPassword(e.target.value)} name="password" className="form-control form-control-sm input-dark" type="password" placeholder="Password" autoComplete="off" />
         </div>
         <div className="col-md-auto">
           <button className="btn btn-success btn-sm">
