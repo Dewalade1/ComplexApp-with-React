5f6ee5d70f6ccc4ea7edce02fa9bc4d7ff277422
Dewa <adewalade@gmail.com>
2020-11-02 15:21:52 +0000

    chore: finish adding tooltips on hover for icons
    added tooltip to search and comment icons in page header
    
    see also: #2, #3

--- src/components/UserLoggedIn.js
+++ src/components/UserLoggedIn.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import ReactTooltip from "react-tooltip";
 
 import DispatchContext from "../DispatchContext";
 import StateContext from "../StateContext";
@@ -19,13 +20,17 @@ function UserLoggedIn(props) {
 
   return (
     <div className="flex-row my-3 my-md-0">
-      <Link to="#" className="text-white mr-2 header-search-icon">
+      <Link data-tip="search" data-for="search" to="#" className="text-white mr-2 header-search-icon">
         <i className="fas fa-search"></i>
       </Link>
-      <span className="mr-2 header-chat-icon text-white">
+      <ReactTooltip id="search" className="custom-tooltip"/>
+      {"  "}
+      <span data-tip="Comment" data-for="comment" className="mr-2 header-chat-icon text-white">
         <i className="fas fa-comment"></i>
         <span className="chat-count-badge text-white"> </span>
       </span>
+      <ReactTooltip id="comment" className="custom-tooltip"/>
+      {"  "}
       <Link to={`/profile/${appState.user.username}`} className="mr-2">
         <img className="small-header-avatar" src={appState.user.avatar} alt="User Avatar" />
       </Link>
