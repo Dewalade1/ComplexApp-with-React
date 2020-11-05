d20aba990f463bf5d76c9a621e9f37612d7928a6
Dewa <adewalade@gmail.com>
2020-11-05 10:19:26 +0000

    chore: added a bug tracker
    added and integrated Sentry bug tracker to code
    
    see also: #2, #3, #4

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('app'));
+Sentry.init({
+  dsn: "https://35b0f590ebec4c888de4178bb72156fa@o471166.ingest.sentry.io/5502827",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 0.8,
+});
+
+ReactDOM.render(<App />, document.getElementById("app"));
 registerServiceWorker();
