5f0f78e11e74541070fabb8b776aeec7567083b5
Dewa <adewalade@gmail.com>
2020-11-07 13:01:59 +0000

    refactor: improve app loading speed
    Lazy load the About, CreatePost, EditPost, ViewSinglePost,
    HomeLoggedIn, PageNotFound, Profile, and Terms components
    
    see also: #2, #3, #4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import Axios from "axios";
 import { useImmerReducer } from "use-immer";
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import StateContext from "./StateContext";
 import DispatchContext from "./DispatchContext";
 
 import "./App.css";
-import About from "./components/About";
-import CreatePost from "./components/CreatePost";
-import EditPost from "./components/EditPost";
 import FlashMessages from "./components/FlashMessages";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import HomeLoggedIn from "./components/HomeLoggedIn";
 import HomeGuest from "./components/HomeGuest";
-import PageNotFound from "./components/PageNotFound";
-import Profile from "./components/Profile";
+import LoadingIcon from "./components/LoadingIcon";
 import Search from "./components/Search";
-import Terms from "./components/Terms";
-import ViewSinglePost from "./components/ViewSinglePost";
+
+// Lazy loading components with React (speeds up loading)
+const About = lazy(() => import("./components/About"));
+const CreatePost = lazy(() => import("./components/CreatePost"));
+const EditPost = lazy(() => import("./components/EditPost"));
+const HomeLoggedIn = lazy(() => import("./components/HomeLoggedIn"));
+const PageNotFound = lazy(() => import("./components/PageNotFound"));
+const Profile = lazy(() => import("./components/Profile"));
+const Terms = lazy(() => import("./components/Terms"));
+const ViewSinglePost = lazy(() => import("./components/ViewSinglePost"));
 
 Axios.defaults.baseURL = "http://localhost:8080";
 
@@ -101,30 +104,32 @@ function App() {
           <div className="App">
             <FlashMessages messages={state.flashMessages} />
             <Header />
-            <Switch>
-              <Route path="/" exact>
-                {state.loggedIn ? <HomeLoggedIn /> : <HomeGuest />}
-              </Route>
-              <Route path="/about-us" exact>
-                <About />
-              </Route>
-              <Route path="/terms">
-                <Terms />
-              </Route>
-              <Route path="/create-post" exact>
-                {state.loggedIn ? <CreatePost /> : <HomeGuest />}
-              </Route>
-              <Route path="/posts/:id/edit" exact>
-                {state.loggedIn ? <EditPost /> : <HomeGuest />}
-              </Route>
-              <Route path="/profile/:username">{state.loggedIn ? <Profile /> : <HomeGuest />}</Route>
-              <Route path="/posts/:id" exact>
-                {state.loggedIn ? <ViewSinglePost /> : <HomeGuest />}
-              </Route>
-              <Route>
-                <PageNotFound />
-              </Route>
-            </Switch>
+            <Suspense fallback={<LoadingIcon />}>
+              <Switch>
+                <Route path="/" exact>
+                  {state.loggedIn ? <HomeLoggedIn /> : <HomeGuest />}
+                </Route>
+                <Route path="/about-us" exact>
+                  <About />
+                </Route>
+                <Route path="/terms">
+                  <Terms />
+                </Route>
+                <Route path="/create-post" exact>
+                  {state.loggedIn ? <CreatePost /> : <HomeGuest />}
+                </Route>
+                <Route path="/posts/:id/edit" exact>
+                  {state.loggedIn ? <EditPost /> : <HomeGuest />}
+                </Route>
+                <Route path="/profile/:username">{state.loggedIn ? <Profile /> : <HomeGuest />}</Route>
+                <Route path="/posts/:id" exact>
+                  {state.loggedIn ? <ViewSinglePost /> : <HomeGuest />}
+                </Route>
+                <Route>
+                  <PageNotFound />
+                </Route>
+              </Switch>
+            </Suspense>
             {state.isSearchOpen ? <Search /> : ""}
             <Footer />
           </div>
